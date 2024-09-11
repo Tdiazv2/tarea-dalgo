74d1777a738fabd05b7e6e97f22be5bbfc70c711
Tdiazv2 <123430733+Tdiazv2@users.noreply.github.com>
2024-09-10 19:11:08 -0500

    Update 11456.py

--- 11456.py
+++ 11456.py
@@ -1,3 +1,4 @@
+import sys
 def trainsorting(n,lista):
     r =[1]*n
     for i in range(1,n):
@@ -12,3 +13,26 @@ def trainsorting(n,lista):
 
 
 print(trainsorting(3,[1,2,3]))
+
+
+if __name__ == "__main__":
+    
+    
+    
+    caso = sys.stdin.readline().strip()
+    contador = 0
+    lista = []
+    no = 0
+    n = 0
+    while caso != "":
+
+        if contador == 0:
+            no = caso
+        elif contador ==1:
+            n =map(int,caso)
+        else:
+
+            lista.extend(map(int, caso.split()))
+        contador +=1
+        caso = sys.stdin.readline().strip()
+    print(trainsorting(n,lista))
